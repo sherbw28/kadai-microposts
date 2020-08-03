c5f50289572843ecbc86fc30c3fc20d1de5cc91d
taro.kirameki <taro.kirameki@techacademy.jp>
2020-08-03 14:29:20 +0000

    follow, unfollow

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @micropost = current_user.microposts.build  # form_with 用
+      @micropost = current_user.microposts.build
       @microposts = current_user.microposts.order(id: :desc).page(params[:page])
     end
   end
